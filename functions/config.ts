0d0bf1184634a6d1e96d4f28f271ba8996de9edc
jansc <janschab@gmail.com>
2021-04-04 09:01:37 +0200

    config

---
+++ functions/config.ts
@@ -0,0 +1,32 @@
+import {ServiceAccount} from "firebase-admin/lib/credential";
+
+export const serviceAccount: ServiceAccount = {
+  "projectId": "actively-796ad",
+  "privateKey": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFA" +
+    "ASCBKkwggSlAgEAAoIBAQDECR/KE8SHqysf\nReLFAEMOorJOywdSkxJTJT+bhSEd34K7K" +
+    "+bQPIsxmd6bfT/SVQkS810waTTTh4Ac\nN4TcJp0rk9O3ym+vExO1MIcKsHUpCS6J1N875" +
+    "u0Dx9A5TVOfFQVINc5Zc32wq8a8\n5DWb223ybJ3TWjqfl+URcy0eY7fe/aLsQp/q6u7Qg" +
+    "NmrbjGaC9H8gGDgEEPnZ8r3\n6l95T7ctOQkPESdmHZ3G5uias2/uNoX3xfomZhP3ZiBg6" +
+    "IEPoZLKTdU6VkXxrJKW\nNen4TpBZIo3Ww8BuDMhlRsGgEUhLkOskX+JUz5DRMtpgvgJpZ" +
+    "MjHOQdrBKMWcpHR\nu1E8rdltAgMBAAECggEADbrN3StRQmc8l+4fGckQ6T9/2YDivGfMi" +
+    "sI7GDtUvHDh\nCRbkDYWQAiNSsfUtuR93iTo+NpvHtWDyGgfRaFHUBC9QTrkA5XQilzgU3" +
+    "SzKp9sa\nxVCBagKcav1dmgXOa3/fZNVP5HfwP6VoP+aD0qyloKr2ysyw+mh7BKPabxxwz" +
+    "k85\nrbuovQSBUd/9Z+y02MnUT666WRTD1TfZcEcFU76HbSW74rWWF9PmcydKlpASYU6Y" +
+    "\nvkOgzqmTIReVK3DyhVfFqHdsvbK8813BUbmRJGRGhMvN9fRJFf7j+qrErwpmJ7l/\n" +
+    "1gLIs6ttFF7gN6ndPdF+5B/2oQWZI2KvlGLJNBIeQQKBgQD+YW5uyNcMUOY360ys\nAhmh" +
+    "Ag25/xzR7toUmXG+ZVNmwD2MIt4LrGotwRK8Hel0kELQlCtV7BYrAb7xuuRD\n+bpvN6EB" +
+    "WNtDWbOboRcC64F/7ZW1B8VDu1NfaVEBc4ZeOWfrTXqo33o0ey6ffHCw\nALOkkJ69w6Ta" +
+    "uGAfNtGHpwc+WQKBgQDFSJtjZyrYNwaJqUHwXz4X6FG+bP3hLx4h\nADBB4s0tFVp34Nnw" +
+    "CB5lQWjEwcy3zeP6RupzBPixJiLfgTMXJLjnGevIxmdXeZeo\nWJIxkMsd+BTHwV8j5CQM" +
+    "3wcTnycDYprNOXfeaSruXLY525jJ5LDg+MIRYRZDrvQN\nmHBV+WlZNQKBgQD9LmQHh4Xi" +
+    "EDLSvTLWkFCJ281lrfA1UhFkJ0T6R0c3IWcgryNp\nYvVbDKtk6DCPf3XAu3hWUF/P9jDh" +
+    "zy2aKlfQkgGBobq50fHdl/ubQGLnvMQgJdfE\njhFxCRVC6IR4C5LOs5K9uY+b5G6G9ggC" +
+    "ZPBnfjlAjhTqTE//QURSS3/+IQKBgQCC\nSktlQLNJDD4jGEdWCnCltWaXDsA0wbtyK/+y" +
+    "46iBh8KxvSvxnEsGsEYS+7LKCQ3x\nMniP8c6UoA0T8u3c8QG0jqfrdyy01rbUFXqnhWq/" +
+    "piGDMzA0Me2lGn4/9TH8yFhY\nBQYnOumBdnVajK7BV2mBiogQlj/v/1Fq0SwPe6KZqQKB" +
+    "gQCP4vwt11y2KLkGD8o9\nRbZCw7tRpdl29YyZ8o+qL/s2f1z173ETGYPUfv5xvfsO3lKq" +
+    "ihN0kCMG2Z2+CWjq\no0uc8YNtkluna5jzjB21rnLsjjzZjwPIkEnu0nCU570pUNy8Mzd2" +
+    "eJXdgvu3yNvH\nHrd4x2k1VV9dHjeoR4KRrI+Cfw==\n-----END PRIVATE KEY-----\n",
+  "clientEmail": "firebase-adminsdk-h0oe2@" +
+    "actively-796ad.iam.gserviceaccount.com",
+};
